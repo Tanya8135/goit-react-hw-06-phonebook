a602ede3049810ce159c1b1ab405fa06ce572e09
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-08-02 09:08:25 +0300

    add lib redux-persist

--- src/components/redux/selectors.js
+++ src/components/redux/selectors.js
@@ -7,10 +7,6 @@ export const getStatusFilter = state => state.filter.status;
 export const getFilteredContacts = createSelector(
     [getContacts, getStatusFilter],
     (contacts, filter) => {
-        // return contacts.filter(contact =>
-        //     contact.name.toLowerCase().includes(filter.toLowerCase())
-        // );
-
         if (!contacts || !Array.isArray(contacts)) {
             return [];
         }
